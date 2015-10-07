2046539bd7689a6d764b13b050760c2cad6dcc41
Roman Jurkov <winfinit@gmail.com>
2015-10-07 15:52:05 -0400

    fixed some linux bugs

--- index.js
+++ index.js
@@ -9,31 +9,28 @@ var DOWNLOAD_URI = "https://fastdl.mongodb.org";
 
 module.exports = function download (opts, cb) {
 
-	var mongo_platform = opts.platform || undefined;
+	var platform = opts.platform || os.platform();;
 
-	if (! mongo_platform ) {
-		var platform = os.platform();
+	var mongo_platform = "";
 
-		var mongo_platform = "";
-
-		switch(platform) {
-			case "darwin":
-				mongo_platform = "osx";
-				break;
-			case "win32":
-				mongo_platform = "win32";
-				break;
-			case "linux":
-				mongo_platform = "linux";
-				break;
-			case "sunos":
-				mongo_platform = "sunos5";
-				break;
-			default:
-				debug("unsupported platform %s by MongoDB", platform);
-				throw new Error("unsupported OS");
-		}
+	switch(platform) {
+		case "darwin":
+			mongo_platform = "osx";
+			break;
+		case "win32":
+			mongo_platform = "win32";
+			break;
+		case "linux":
+			mongo_platform = "linux";
+			break;
+		case "sunos":
+			mongo_platform = "sunos5";
+			break;
+		default:
+			debug("unsupported platform %s by MongoDB", platform);
+			throw new Error("unsupported OS");
 	}
+
 	debug("selected platform %s", mongo_platform);
 	DOWNLOAD_URI += "/" + mongo_platform;
 
@@ -118,81 +115,82 @@ module.exports = function download (opts, cb) {
 			} else {
 				throw new Error("unsupported linux distribution");
 			}
-			continueProcess();
 			name += "-" + mongo_version;
+			continueProcess();
 		});
 	} else {
 		name += "-" + mongo_version;
 		continueProcess();
 	}
 
+	function continueProcess() {
+		var mongo_archive = "";
+		if ( mongo_platform === "win32" ) {
+			mongo_archive = "zip";
+		} else {
+			mongo_archive = "tgz";
+		}
+		debug("selected archive %s", mongo_archive);
+
+		name += "." + mongo_archive;
+		debug("final name: %s", name);
+		DOWNLOAD_URI += "/" + name;
+		debug("final download URI: %s", DOWNLOAD_URI);
+
+		var temp_dir = opts.download_dir || os.tmpdir();
+		var download_dir = path.resolve(temp_dir, 'mongodb-download');
+		// create dir
+
+		if (!fs.existsSync(download_dir)){
+	    	fs.mkdirSync(download_dir);
+		}
+
+		debug("download directory: %s", temp_dir);
+		var download_location = path.resolve(download_dir, name);
+		
+		var temp_download_location = path.resolve(download_dir, name + ".in_progress");
+		debug("download complete path: %s", download_location);
+
+		if ( fs.existsSync(download_location) ) {
+			debug("sending file from cache");
+			cb(null, download_location);
+		} else {
+			var file = fs.createWriteStream(temp_download_location);
+			var request = http.get(DOWNLOAD_URI, function(response) {
+				var cur = 0;
+				var len = parseInt(response.headers['content-length'], 10);
+				var total = len / 1048576;
+
+			  	response.pipe(file);
+			  	file.on('finish', function() {
+		      		file.close(function() {
+		      			fs.renameSync(temp_download_location, download_location);
+		      			cb(null, download_location);
+		      		});
+		    	});
+
+		  		response.on("data", function(chunk) {
+			        cur += chunk.length;
+			        var percent_complete = (100.0 * cur / len).toFixed(1);
+			        var mb_complete = (cur / 1048576).toFixed(1);
+			        var text_to_print = "Completed: " + percent_complete + 
+			        	"% (" + mb_complete + "mb / " + total.toFixed(1) + "mb)" + 
+			        	cr_return;
+			        process.stdout.write(text_to_print);
+	        	});
+
+		        request.on("error", function(e){
+		        	debug("request error:", e);
+		        	cb(e, null);
+		        });
+			});
+
+		}
+	}
 
 }
 
-function continueProcess() {
-	var mongo_archive = "";
-	if ( mongo_platform === "win32" ) {
-		mongo_archive = "zip";
-	} else {
-		mongo_archive = "tgz";
-	}
-	debug("selected archive %s", mongo_archive);
 
-	name += "." + mongo_archive;
-	debug("final name: %s", name);
-	DOWNLOAD_URI += "/" + name;
-	debug("final download URI: %s", DOWNLOAD_URI);
-
-	var temp_dir = opts.download_dir || os.tmpdir();
-	var download_dir = path.resolve(temp_dir, 'mongodb-download');
-	// create dir
-
-	if (!fs.existsSync(download_dir)){
-    	fs.mkdirSync(download_dir);
-	}
-
-	debug("download directory: %s", temp_dir);
-	var download_location = path.resolve(download_dir, name);
-	
-	var temp_download_location = path.resolve(download_dir, name + ".in_progress");
-	debug("download complete path: %s", download_location);
-
-	if ( fs.existsSync(download_location) ) {
-		debug("sending file from cache");
-		cb(null, download_location);
-	} else {
-		var file = fs.createWriteStream(temp_download_location);
-		var request = http.get(DOWNLOAD_URI, function(response) {
-			var cur = 0;
-			var len = parseInt(response.headers['content-length'], 10);
-			var total = len / 1048576;
-
-		  	response.pipe(file);
-		  	file.on('finish', function() {
-	      		file.close(function() {
-	      			fs.renameSync(temp_download_location, download_location);
-	      			cb(null, download_location);
-	      		});
-	    	});
-
-	  		response.on("data", function(chunk) {
-		        cur += chunk.length;
-		        var percent_complete = (100.0 * cur / len).toFixed(1);
-		        var mb_complete = (cur / 1048576).toFixed(1);
-		        var text_to_print = "Completed: " + percent_complete + 
-		        	"% (" + mb_complete + "mb / " + total.toFixed(1) + "mb)" + 
-		        	cr_return;
-		        process.stdout.write(text_to_print);
-        	});
-
-	        request.on("error", function(e){
-	        	debug("request error:", e);
-	        	cb(e, null);
-	        });
-		});
-
-	}
-}
 				
 
 /*
